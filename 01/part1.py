0aa2ac9ef7ce28aa22ba65099c60d15a42297d21
Kaqtus14 <kaqtus14@gmail.com>
2021-12-04 15:43:30 +0100

    Reorganize

--- 01/part1.py
+++
@@ -1,9 +0,0 @@
-with open("input.txt") as f:
-    data = [int(x) for x in f.read().strip().split("\n")]
-
-larger = 0
-for i in range(1, len(data)):
-    if data[i-1] < data[i]:
-        larger += 1
-
-print(larger)
