a072ece046b09ba7e714dfad2a540c89fa0871a6
Kaqtus14 <kaqtus14@gmail.com>
2021-12-07 17:08:53 +0100

    Solve day 7 part 2

--- 07/main.py
+++ 07/main.py
@@ -3,17 +3,20 @@ def get_spent_fuel(i, data):
 
     for d in data:
         if d < i:
-            fuel += i - d
+            for j in range(1, i - d + 1):
+                fuel += j
         elif d > i:
-            fuel += d - i
+            for j in range(1, d - i + 1):
+                fuel += j
     return fuel
 
 
 with open("input.txt") as f:
     data = [int(x) for x in f.read().split(",")]
 
-lowest = (-1, 999999)
+lowest = (-1, 999999999999999)
 for i in range(2000):
+    print(i)
     fuel = get_spent_fuel(i, data)
     if fuel < lowest[1]:
         lowest = (i, fuel)
