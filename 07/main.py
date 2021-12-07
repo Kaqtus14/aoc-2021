92d0edd0a737e38404cc8da4f5fbf5e6a5060c76
Kaqtus14 <kaqtus14@gmail.com>
2021-12-07 20:08:57 +0100

    Get value using mean instead of bruteforcing :P

--- 07/main.py
+++ 07/main.py
@@ -14,11 +14,6 @@ def get_spent_fuel(i, data):
 with open("input.txt") as f:
     data = [int(x) for x in f.read().split(",")]
 
-lowest = (-1, 999999999999999)
-for i in range(2000):
-    print(i)
-    fuel = get_spent_fuel(i, data)
-    if fuel < lowest[1]:
-        lowest = (i, fuel)
-
-print(lowest)
+i = int(sum(data) / len(data))
+fuel = get_spent_fuel(i, data)
+print(fuel)
