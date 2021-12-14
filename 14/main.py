0ebec86c59761caf4d32d403d019eea5e63430c6
Kaqtus14 <kaqtus14@gmail.com>
2021-12-14 20:41:12 +0100

    I DID IT!

--- 14/main.py
+++ 14/main.py
@@ -1,24 +1,42 @@
 from collections import Counter
 
 
+def add(template, key, amount):
+    if key not in template:
+        template[key] = 0
+    template[key] += amount
+    return template
+
+
+def flatten_template(template):
+    flat = {}
+    for k, v in template.items():
+        flat = add(flat, k[0], v)
+    return flat
+
+
 def step(template, pairs):
-    new = []
+    new = {}
 
-    for i in range(len(template)-1):
-        t = (template[i], template[i+1])
+    for k, v in template.items():
+        if tuple(k) in pairs:
+            n = pairs[tuple(k)]
+            new = add(new, k[0]+n, v)
+            new = add(new, n+k[1], v)
+        else:
+            new = add(new, k, v)
 
-        new.append(template[i])
-        if t in pairs:
-            new.append(pairs[t])
-
-    new.append(template[i+1])
     return new
 
 
 with open("input.txt") as f:
     parts = f.read().split("\n\n")
 
-    template = parts[0]
+    template = {}
+    temp = parts[0]
+    for i in range(len(temp)-1):
+        l = temp[i]+temp[i+1]
+        template = add(template, l, 1)
 
     pairs = {}
     for line in parts[1].split("\n"):
@@ -26,8 +44,10 @@ with open("input.txt") as f:
         pairs[tuple(s[0])] = s[1]
 
 
-for _ in range(10):
+for _ in range(40):
     template = step(template, pairs)
 
+template = flatten_template(template)
 d = sorted(dict(Counter(template)).values())
-print(d[-1]-d[0])
+res = d[-1]-d[0]-2
+print(res)
